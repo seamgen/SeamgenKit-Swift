d3b4c807eed7b954fc6ab31921f112bd7141a106
Sam Gerardi <sgerardi@seamgen.com>
2016-12-13 11:28:20 -0800

    Add hex initializer to UIColor.

--- SeamgenKit/Classes/UIColor+Extensions.swift
+++ SeamgenKit/Classes/UIColor+Extensions.swift
@@ -26,5 +26,36 @@ extension UIColor {
         
         self.init(red: CGFloat(r)/255.0, green: CGFloat(g)/255.0, blue: CGFloat(b)/255.0, alpha: alpha)
     }
+    
+    
+    /// Initializes the color with a hex string.
+    ///
+    /// - Parameter string: A hex string with the format #DEF, #FF00FF, #FF00FF00.  The '#' is not required.
+    public convenience init(hex string: String) {
+        var hex = string.hasPrefix("#")
+            ? String(string.characters.dropFirst())
+            : string
+        
+        guard hex.characters.count == 3 || hex.characters.count == 6 || hex.characters.count == 8 else {
+            self.init(white: 0, alpha: 0)
+            return
+        }
+        
+        if hex.characters.count == 3 {
+            for (index, char) in hex.characters.enumerated() {
+                hex.insert(char, at: hex.characters.index(hex.startIndex, offsetBy: index * 2))
+            }
+        }
+        
+        if hex.characters.count == 6 {
+            hex += "FF"
+        }
+        
+        self.init(
+            red:    CGFloat((uint(hex, radix: 16)! >> 24) & 0xFF) / 255.0,
+            green:  CGFloat((uint(hex, radix: 16)! >> 16) & 0xFF) / 255.0,
+            blue:   CGFloat((uint(hex, radix: 16)! >> 8) & 0xFF) / 255.0,
+            alpha:  CGFloat((uint(hex, radix: 16)!) & 0xFF) / 255.0)
+    }
 }
 
