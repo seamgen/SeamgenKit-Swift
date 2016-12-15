3742583c0b6fce8c5660376db983abd89e2626fc
Sam Gerardi <sgerardi@seamgen.com>
2016-12-15 11:23:09 -0800

    Add scaling extensions.

--- SeamgenKit/Classes/UI/UIImage+Extensions.swift
+++ SeamgenKit/Classes/UI/UIImage+Extensions.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 
 extension UIImage {
@@ -28,3 +29,49 @@ extension UIImage {
         self.init(cgImage: image.cgImage!)
     }
 }
+
+
+extension UIImage {
+    
+    /// Scales the image to a given size.
+    ///
+    /// - Parameters:
+    ///   - size:       The new size of the image.
+    ///   - hasAlpha:   Include an alpha channel.
+    /// - Returns: The scaled image.
+    func scaledTo(size: CGSize, hasAlpha: Bool = false) -> UIImage {
+        let image = self
+        let scale: CGFloat = image.scale
+        
+        UIGraphicsBeginImageContextWithOptions(size, !hasAlpha, scale)
+        image.draw(in: CGRect(origin: .zero, size: size))
+        
+        let scaledImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return scaledImage!
+    }
+    
+    /// Scales the image to fit within a given size while maintaining the aspect ratio.
+    ///
+    /// - Parameters:
+    ///   - size: The maximum size of the scaled image.
+    ///   - hasAlpha: Include an alpha channel.
+    /// - Returns: The scaled image.
+    func scaledTo(fitSize size: CGSize, hasAlpha: Bool = false) -> UIImage {
+        let newSize = self.size.aspectFit(to: size)
+        return self.scaledTo(size: newSize)
+    }
+    
+    /// Scales the image to fill a given size whil maintaining the aspect ratio.
+    ///
+    /// - Parameters:
+    ///   - size: The minimum size of the scaled image.
+    ///   - hasAlpha: Include an alpha channel.
+    /// - Returns: The scaled image.
+    func scaledTo(fillSize size: CGSize, hasAlpha: Bool = false) -> UIImage {
+        let newSize = self.size.aspectFill(to: size)
+        return self.scaledTo(size: newSize)
+    }
+}
+
