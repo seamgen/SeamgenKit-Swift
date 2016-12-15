480e85b63146f0165ee4ef8a96a4c01267bb5f23
Sam Gerardi <sgerardi@seamgen.com>
2016-12-15 13:30:42 -0800

    Add TouchID prompt implementation.

--- SeamgenKit/Classes/UI/UIDevice+Extensions.swift
+++ SeamgenKit/Classes/UI/UIDevice+Extensions.swift
@@ -13,8 +13,47 @@ import LocalAuthentication
 extension UIDevice {
     
     /// Returns true if Touch ID is available on this device.
-    var isTouchIDAvailable: Bool {
+    @available(iOS 9.3, *)
+    public var isTouchIDAvailable: Bool {
         return LAContext().canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: nil)
+    }
+    
+    /// Presents the Touch ID prompt to the user.
+    ///
+    /// - Parameters:
+    ///   - localizedReason: The reason the prompt is being presented.
+    ///   - completion: `success` true if authentication succeeded.  `error` has a value if validation failed or was cancelled.
+    @available(iOS 9.3, *)
+    public func authenticateWithTouchID(_ localizedReason: String, completion: @escaping (_ success: Bool, _ error: Error?) -> Void) {
+        DispatchQueue.global(qos: .default).async {
+            let context = LAContext()
+            let policy: LAPolicy = .deviceOwnerAuthenticationWithBiometrics
+            
+            do {
+                if try context.canEvaluatePolicy(policy, error: nil) {
+                    // Evaluate the policy...
+                    context.evaluatePolicy(policy, localizedReason: localizedReason) { success, error in
+                        // Policy has been evaluated.
+                        DispatchQueue.main.async {
+                            completion(success, error)
+                        }
+                        return
+                    }
+                } else {
+                    // Did not pass evaluation.
+                    DispatchQueue.main.async {
+                        completion(false, nil)
+                    }
+                    return
+                }
+            } catch {
+                // An error ocurred.
+                DispatchQueue.main.async {
+                    completion(false, error)
+                }
+                return
+            }
+        }
     }
 }
 
