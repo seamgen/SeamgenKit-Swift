afa1542c67216aa316ca924a4b737fd3db6688c9
Sam Gerardi <sgerardi@seamgen.com>
2016-12-15 12:50:44 -0800

    Add rotation methods.

--- SeamgenKit/Classes/UI/UIImage+Extensions.swift
+++ SeamgenKit/Classes/UI/UIImage+Extensions.swift
@@ -86,3 +86,92 @@ extension UIImage {
     }
 }
 
+
+// MARK: - Rotation
+
+/// Defines an image rotation transform.
+///
+/// - none:         Don't rotate the image.
+/// - left:         Rotate the image left (-90 degrees)
+/// - right:        Rotate the image right (90 degrees)
+/// - byDegrees:    Rotate the image a given number of degrees.
+/// - byRadians:    Rotate the image a given number of radians.
+public enum UIImageRotation {
+    case none
+    case left
+    case right
+    case byDegrees(Double)
+    case byRadians(Double)
+    
+    /// Returns the rotation in radians.
+    internal var radians: Double {
+        switch self {
+        case .none:                     return (0.0).degreesToRadians
+        case .left:                     return (-90.0).degreesToRadians
+        case .right:                    return (90.0).degreesToRadians
+        case .byDegrees(let degrees):   return degrees.degreesToRadians
+        case .byRadians(let radians):   return radians
+        }
+    }
+}
+
+
+extension UIImage {
+    
+    
+    /// Returns the image rotated to a given degree.
+    ///
+    /// - Parameters:
+    ///   - rotation:   The rotation to apply.
+    ///   - flipped:    If the image should be flipped.
+    /// - Returns: The rotated image.
+    public func rotated(_ rotation: UIImageRotation, flipped: Bool = false) -> UIImage {
+        if #available(iOS 10.0, *) {
+            return iOS10_rotated(rotation, flipped: flipped)
+        } else {
+            return iOS9_rotated(rotation, flipped: flipped)
+        }
+    }
+    
+    @available(*, deprecated: 10.0)
+    private func iOS9_rotated(_ rotation: UIImageRotation, flipped: Bool = false) -> UIImage {
+        let radians = CGFloat(rotation.radians)
+        let transform = CGAffineTransform(rotationAngle: radians)
+        var rect = CGRect(origin: .zero, size: self.size).applying(transform)
+        rect.origin = .zero
+        
+        UIGraphicsBeginImageContext(rect.size)
+        defer { UIGraphicsEndImageContext() }
+        
+        guard let context = UIGraphicsGetCurrentContext() else { fatalError("Failed to create graphics context.") }
+        guard let cgImage = cgImage else { fatalError("Failed to get CGImage from \(self)") }
+        
+        context.translateBy(x: rect.size.width / 2.0, y: rect.size.height / 2.0)
+        context.rotate(by: radians)
+        context.scaleBy(x: flipped ? -1 : 1, y: -1.0)
+        context.draw(cgImage, in: CGRect(x: -size.width / 2, y: -size.height / 2, width: size.width, height: size.height))
+        
+        return UIGraphicsGetImageFromCurrentImageContext()!
+    }
+    
+    @available(iOS 10.0, *)
+    private func iOS10_rotated(_ rotation: UIImageRotation, flipped: Bool = false) -> UIImage {
+        guard let cgImage = self.cgImage else { fatalError("Failed to get CGImage from \(self)") }
+        
+        let radians = CGFloat(rotation.radians)
+        let transform = CGAffineTransform(rotationAngle: radians)
+        var rect = CGRect(origin: .zero, size: self.size).applying(transform)
+        rect.origin = .zero
+        
+        let renderer = UIGraphicsImageRenderer(size: rect.size)
+        return renderer.image { renderContext in
+            renderContext.cgContext.translateBy(x: rect.midX, y: rect.midY)
+            renderContext.cgContext.rotate(by: radians)
+            renderContext.cgContext.scaleBy(x: flipped ? -1.0 : 1.0, y: -1.0)
+            
+            let drawRect = CGRect(origin: CGPoint(x: -self.size.width/2, y: -self.size.height/2), size: self.size)
+            renderContext.cgContext.draw(cgImage, in: drawRect)
+        }
+    }
+}
+
